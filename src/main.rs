befa12afca06a0ee8a7005871556ef52ae165759
Germain GAU <germain.gau@c-s.fr>
2018-03-21 15:28:57 +0100

    parallel computing using rayon

--- src/main.rs
+++ src/main.rs
@@ -39,18 +39,22 @@ fn to_rgb_color(point: MnIteration) -> image::Rgb<u8> {
     let n = point.zn.norm();
     if n < 2.0 { image::Rgb::<u8>::from_channels(0, 0, 0, 0) }
     else {
-        let c = ((1.0 - (1.0 / (point.step as f64))) * 255.0) as u8;
+        // let c = ((1.0 - (1.0 / (point.step as f64))) * 255.0) as u8;
+        let c = (
+            point.step as f64 -
+            (point.zn.norm().log(10.0) / 2.0_f64.log(10.0)).log(2.0)
+        ) as u8;
         image::Rgb::<u8>::from_channels(c, c, c, 0)
     }
 }
 
 fn main() {
-    let wx = 3840;
-    let wy = 2160;
+    let wx = 36000;
+    let wy = 20250;
     // let wx = 1280;
     // let wy = 720;
 
-    let range_re = 1.75;
+    let range_re = 3.5;
     let range_im = range_re * ((wy as f64) / (wx as f64));
 
     let off_re = -0.75;
@@ -61,12 +65,12 @@ fn main() {
 
     let mut pb = ProgressBar::new(wx);
 
-    // let mut imgbuf: image::RgbImage = image::ImageBuffer::new(wx, wy);
     let pixels : Vec<Vec<image::Rgb<u8>>> = (0..wx).map(
         move |ox| {
             let x = ox.to_owned();
             pb.inc();
-            (0..wy).collect::<Vec<u64>>().par_iter().map( move |oy| {
+            if x == wx - 1 {pb.finish_print("Done, exporting image.")}
+            (0..wy).collect::<Vec<u64>>().par_iter().map(move |oy| {
                 let y = oy.to_owned();
                 to_rgb_color(
                     mandelbrot(
@@ -74,28 +78,28 @@ fn main() {
                             off_re + ((x as f64) * step_re) - range_re,
                             off_im + ((y as f64) * step_im) - range_im
                         ),
-                        2000
+                        5000
                     )
                 )
             }).collect()
         }
     ).collect();
-    println!("{:?}", pixels);
-    // for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-    //     *pixel = to_rgb_color(
-    //         mandelbrot(
-    //             Complex64::new(
-    //                 off_re + ((x as f64) * step_re) - range_re,
-    //                 off_im + ((y as f64) * step_im) - range_im
-    //             ),
-    //             2000
-    //         )
-    //     );
-    //     println!("({}, {})", x, y);
-    // }
-    //
-    // println!("Hello, world!");
-    // let ref mut fout = File::create("fractal.png").unwrap();
-    // image::ImageRgb8(imgbuf).save(fout, image::PNG).unwrap();
+
+    let mut imgbuf: image::RgbImage = image::ImageBuffer::new(wx as u32, wy as u32);
+
+    (0..wx).collect::<Vec<u64>>().iter().map(
+        |ox| {
+            let x = ox.to_owned();
+            (0..wy).collect::<Vec<u64>>().iter().map( |oy| {
+                let y = oy.to_owned();
+                imgbuf.put_pixel(x as u32, y as u32, pixels[x as usize][y as usize].to_owned());
+                ()
+            }).collect::<()>();
+            ()
+        }
+    ).collect::<()>();
+
+    let ref mut fout = File::create("fractal.png").unwrap();
+    image::ImageRgb8(imgbuf).save(fout, image::PNG).unwrap();
 
 }
