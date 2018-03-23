453f7c804812a191e4f9ce99a6767a4f1b01f6e6
Germain <germain.gau@gmail.com>
2018-03-23 21:25:21 +0100

    added seed and windows real width in arguments

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate itertools;
 extern crate num_complex;
 extern crate image;
 extern crate rayon;
+extern crate regex;
 
 // extern crate pbr;
 
@@ -17,6 +18,7 @@ use clap::App;
 use window::{ImageWindow, Window};
 use std::fs::File;
 use num_complex::Complex64;
+use regex::Regex;
 
 fn parse_dimensions(fmt: &str) -> Option<(u32, u32)> {
     match fmt.split("x").collect::<Vec<&str>>().as_slice() {
@@ -29,6 +31,20 @@ fn parse_dimensions(fmt: &str) -> Option<(u32, u32)> {
     })
 }
 
+fn parse_seed(fmt: &str) -> Option<Complex64> {
+    let re = Regex::new(r"\(\s?(.*)\s?,\s?(.*)\s?\)").unwrap();
+    for cap in re.captures_iter(fmt) {
+        println!("Parse seed: x: {}, y: {}", &cap[1], &cap[2]);
+        return cap[1].parse::<f64>().and_then(|re| {
+            cap[2].parse::<f64>().and_then(|im| {
+                Ok(Some(Complex64::new(re, im)))
+            })
+
+        }).unwrap_or(None);
+    }
+    None
+}
+
 fn main() {
 
     let yaml = load_yaml!("cli.yaml");
@@ -38,6 +54,8 @@ fn main() {
     let aa = matches.value_of("antialiasing").unwrap_or("1").parse::<u32>().expect("Expected number for anti aliasing");;
     let iterations = matches.value_of("iterations").unwrap_or("1000").parse::<u64>().expect("Expected number for iterations");
     let output_file = matches.value_of("output").unwrap_or("fractal.png");
+    let seed = parse_seed(matches.value_of("seed").unwrap_or("(-2.5, 1.25)")).expect("Seed format: '(re, im)'");
+    let camera_width = matches.value_of("camera_width").unwrap_or("4.5").parse::<f64>().expect("Expected a float number as camera width");
     dims_o.expect("Expected format <num>x<num>, e.g: 1920x1080");
     let dims = dims_o.unwrap();
     println!("Resolution     : {:?}", dims);
@@ -45,7 +63,7 @@ fn main() {
     println!("Max iterations : {:?}", iterations);
 
     let mut window : ImageWindow = Window::new(dims.0, dims.1, iterations);
-    window.scope(Complex64::new(-2.5, 1.25), 4.5);
+    window.scope(seed, camera_width);
     window.set_antialiasing(aa);
     let imgbuf = window.fill();
 
